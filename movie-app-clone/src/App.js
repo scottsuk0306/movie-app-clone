c7165ada882ec331b812472199748dfce94d279c
Scott Suk <scottsuk0306@gmail.com>
2022-08-17 18:47:15 +0900

    Implement about page

--- movie-app-clone/src/App.js
+++ movie-app-clone/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import Home from './routes/Home';
 import './App.css';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import About from './routes/About';
 
 function App() {
-  return <Home />
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </HashRouter>
+  );
 }
 
 export default App;
