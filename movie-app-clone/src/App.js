53aadb6b5391d27f7bc24f450918f0a2178f5f21
Scott Suk <scottsuk0306@gmail.com>
2022-08-17 18:24:48 +0900

    Apply CSS

--- movie-app-clone/src/App.js
+++ movie-app-clone/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import Movie from './Movie'
+import './App.css'
 
 class App extends React.Component {
   state = {
@@ -24,13 +25,13 @@ class App extends React.Component {
 
   render() {
     const { isLoading, movies } = this.state;
-    return (<section class="container">
+    return (<section className="container">
       {isLoading ? (
-        <div class="loader">
-          <span class="loader__text">Loading...</span>
+        <div className="loader">
+          <span className="loader__text">Loading...</span>
         </div>
       ) : (
-        <div class="movies">
+        <div className="movies">
           {movies.map((movie) => (
             <Movie
               key={movie.id}
@@ -39,6 +40,7 @@ class App extends React.Component {
               title={movie.title}
               summary={movie.summary}
               poster={movie.medium_cover_image}
+              genres={movie.genres}
             />
           ))}
         </div>
